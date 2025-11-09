dafa503af2b2fe72f46ec35efd5fb83fb0e96e9f
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-09 05:52:54 +0000

    add logo https://www.dropbox.com/scl/fi/5nsta4zu77z4vbcto440p/0001-13571

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,24 +1,10 @@
-import type { SVGProps } from 'react';
+import { cn } from "@/lib/utils";
+import type { HTMLAttributes } from "react";
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 120 30"
-      width="120"
-      height="30"
-      {...props}
-    >
-      <text
-        x="0"
-        y="23"
-        fontFamily="Roboto, sans-serif"
-        fontSize="24"
-        fontWeight="700"
-        fill="currentColor"
-      >
-        Emphz
-      </text>
-    </svg>
+    <div className={cn("text-2xl font-bold font-headline", className)} {...props}>
+      Emphz
+    </div>
   );
 }
