bcbcb1492a0f4d2e456142c51c5867569f4845b8
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-18 15:23:33 +0000

    18:22:08.302 Running build in Washington, D.C., USA (East) – iad1 18:22:

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,8 +3,8 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -19,12 +19,6 @@ export function initializeFirebase() {
 export function getSdks(firebaseApp: FirebaseApp) {
   const auth = getAuth(firebaseApp);
   const firestore = getFirestore(firebaseApp);
-
-  if (process.env.NODE_ENV === 'development') {
-    // Uncomment the following lines to use the local emulators
-    // connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
-    // connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
-  }
 
   return {
     firebaseApp,
