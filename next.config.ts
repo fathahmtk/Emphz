7b4f56103d6f608d00f4078db7baacd2fecffd93
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-18 15:27:31 +0000

    forward-logs-shared.ts:95 Download the React DevTools for a better devel

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
