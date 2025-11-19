c0989ced01b264b9fa1bf37295eb5542360b90de
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-19 15:10:18 +0000

    Code
    GET https://9000-firebase-studio-1762661065747.cluster-3gc7bglotjg

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const logoUrls = {
   default: "https://lh3.googleusercontent.com/pw/AP1GczNRnzYjFyAIBA_0WpIyH7FHZ5OJ7H_nadGQFV4dntfPUeTCzzBPO2vXJNWzafExXzvab-w2GFEsrNVCos-tz1xActbH-ZowKNCrPMCvUTI9B-UPSqBO8Ff_ZPlE3OoOOlcJWPceaQtzmHUNC1iK2dtoGQ=w879-h879-s-no-gm?authuser=0",
-  white: "https://lh3.googleusercontent.com/pw/AP1GczMFf6QCG8H2D5-b82yWf5Q24t_jS8g3o-m4T7G5P2B-M9pE6y3LwGvF8d2b_JqW9o1vJgZqY4eR2wXzXl6Xn5zV7qR9aG-gA=w879-h879-s-no-gm?authuser=0"
+  white: "https://storage.googleapis.com/studio-assests/projects/emhz/logo-white.png"
 }
 
 export function Logo({ 
