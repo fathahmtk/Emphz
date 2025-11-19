9e0cff129aa18a3c9836b63816b37c313fb32b4c
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-19 00:10:02 +0000

    https://photos.fife.usercontent.google.com/pw/AP1GczNZPlWYkQg6TaSceGWIC-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
     <>
       <SiteHeader />
       <main className="flex-1">
-        <section className="relative h-dvh w-full flex items-center justify-between text-left overflow-hidden">
+        <section className="relative h-dvh w-full flex items-center justify-center text-left overflow-hidden">
           <HeroCarousel images={heroImages} />
           <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent z-10" />
           <div className="container relative flex items-center justify-between px-4 md:px-6 z-20">
@@ -376,6 +376,8 @@ export default function Home() {
     </>
   );
 }
+
+    
 
     
 
