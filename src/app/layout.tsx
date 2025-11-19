afa31b82b76f279e8589079826205dffeb95e2c1
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-19 13:12:08 +0000

    NEXT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase';
-import { ClientOnly } from '@/components/client-only';
 import { BackToTopButton } from '@/components/ui/back-to-top-button';
 
 const roboto = Roboto({
@@ -38,11 +37,9 @@ export default function RootLayout({
       <head />
       <body className={cn('min-h-screen font-body antialiased', roboto.variable, roboto_headline.variable)}>
         <FirebaseClientProvider>
-          <ClientOnly>
             <div className="relative flex min-h-dvh flex-col bg-background">
                 {children}
             </div>
-          </ClientOnly>
         </FirebaseClientProvider>
         <Toaster />
         <BackToTopButton />
