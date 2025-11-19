6cfe6e9cf574413e0db9ba40192aed4efe656a9a
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-19 02:54:11 +0000

    main-app.js?v=1763520684194:1825 Download the React DevTools for a bette

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,6 +14,7 @@ export function Logo({ className, imageClassName, ...props }: HTMLAttributes<HTM
         fill
         className={cn("object-contain", imageClassName)}
         priority
+        unoptimized
       />
     </div>
   );
