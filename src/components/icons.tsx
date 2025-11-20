b145d5f0a9ffdf01af9781fe3bbae5f26cec2005
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-20 14:11:49 +0000

    DUPLICATED LOGO

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,7 +7,7 @@ export function Logo({
   variant = 'default',
   ...props
 }: HTMLAttributes<HTMLElement> & { variant?: 'default' | 'white' }) {
-  const fillColor = variant === 'white' ? '#FFFFFF' : '#102A27';
+  const fillColor = variant === 'white' ? '#FFFFFF' : 'hsl(var(--foreground))';
 
   return (
     <div
@@ -17,7 +17,7 @@ export function Logo({
       <svg
         width="100%"
         height="100%"
-        viewBox="0 0 450 100"
+        viewBox="0 0 340 100"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="xMidYMid meet"
@@ -27,21 +27,8 @@ export function Logo({
           d="M0 0 H40 V15 H15 V42.5 H35 V57.5 H15 V85 H40 V100 H0 V0 Z M50 0 L25 50 L50 50 L30 100 L55 50 L30 50 Z"
           fill={fillColor}
         />
-        <path 
-          d="M27.5 50 L50 50 L30 100 L55 48 L30 48Z" 
-          fill={fillColor}
-        />
-
-        {/* Text: Emphz */}
-        <path
-          d="M80 15 H120 M80 50 H120 M80 85 H120 M140 100 V15 H165 L180 50 L165 85 H140 M190 100 V15 H210 C230 15 230 40 210 40 H190 M240 100 V15 H255 V85 H280 V100 H240 M290 15 L320 85 H300 L295 70 H275 L270 85 H250 L280 15 H290 Z M285 55 L278 35 L271 55 H285"
-          stroke={fillColor}
-          strokeWidth="15"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-         <text x="135" y="88" fontFamily="sans-serif" fontSize="80" fill={fillColor} fontWeight="bold">
-            <tspan>m</tspan><tspan>p</tspan><tspan>h</tspan><tspan>z</tspan>
+        <text x="75" y="85" fontFamily="sans-serif" fontSize="80" fill={fillColor} fontWeight="bold">
+            <tspan>mphz</tspan>
         </text>
       </svg>
     </div>
